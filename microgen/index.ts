2946e34eabb9435099f3f45b1bc95f2857f6dc38
Saefulloh Maslul <maslulsaefulloh@gmail.com>
2020-03-26 14:01:06 +0700

    [ENHANCEMENT] Create Exec Bash from Node
    Add execution bash script from node js

--- microgen/index.ts
+++ microgen/index.ts
@@ -1,4 +1,22 @@
+import cp from 'child_process'
+import { join } from 'path'
+import { existsSync, mkdirSync } from 'fs'
+import logger from './lib/debugger'
+
 import Generator from './generator'
 
-const generator = new Generator()
-generator.createClassServices()
+const main = () => {
+  try {
+    if (!existsSync(join(__dirname, '..', 'src'))) mkdirSync(join(__dirname, '..', 'src'))
+
+    cp.exec(`chmod +x ${join(__dirname, '..', 'scripts', 'generate-protoc.sh')}`)
+    cp.exec(join(__dirname, '..', 'scripts', 'generate-protoc.sh'))
+
+    const generator = new Generator()
+    generator.createClassServices()
+  } catch (error) {
+    logger.log(`Error: ${error}`)
+  }
+}
+
+main()
