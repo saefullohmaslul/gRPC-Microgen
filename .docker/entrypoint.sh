2b7781112de584abaee056db89a838c27c2a3a69
Saefulloh Maslul <maslulsaefulloh@gmail.com>
2020-04-04 05:48:28 +0700

    [REFACTOR] Change image from alpine to jessie

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-cp -r /cache/node_modules/. /app/node_modules/
-
-exec npm-run-all generate:server server:dev
+exec npm run start:dev
--- scripts/generate-protoc.ts
+++ scripts/generate-protoc.ts
@@ -22,15 +22,14 @@ export const generateProto = () => {
     if (!existsSync(join(PROTO_DEST, `${FILE}`))) {
       mkdirSync(join(PROTO_DEST, `${FILE}`))
     }
-    cp.execSync(`./scripts/protoc \
-    --plugin=protoc-gen-grpc=./node_modules/protoc-gen-grpc-ts/bin/grpc_node_plugin \
+
+    cp.execSync(`protoc-gen-grpc \
     --js_out=import_style=commonjs,binary:${PROTO_DEST}/${FILE} \
     --grpc_out=${PROTO_DEST}/${FILE} \
     --proto_path ./schema \
     ${file}`)
 
-    cp.execSync(`./scripts/protoc \
-    --plugin=protoc-gen-ts=./node_modules/.bin/protoc-gen-ts-plugin \
+    cp.execSync(`protoc-gen-grpc-ts \
     --ts_out=service=true:${PROTO_DEST}/${FILE} \
     --proto_path ./schema \
     ${file}`)
